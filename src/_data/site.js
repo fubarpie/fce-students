8a6b742db6de4e3c151b75a46f6856cea9216db6
fubarpie <39785359+fubarpie@users.noreply.github.com>
2025-07-19 17:58:05 +0000

    Create site.js

---
+++ src/_data/site.js
@@ -0,0 +1,9 @@
+// This file holds global data for your site.
+// You can access it in templates like this: {{ site.title }}
+
+module.exports = {
+  title: "FCE Student Profiles",
+  description: "Meet the talented students of the FCE program.",
+  // The URL for your site, used for building absolute URLs
+  url: "https://your-github-username.github.io/fce-students/", 
+};
